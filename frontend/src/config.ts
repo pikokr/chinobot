f58e4e77ef409d3ed4db9e1144d0292b59b55b5e
pikokr <parangee9706@gmail.com>
2020-10-12 23:24:06 +0900

    [Edit] Change dev address

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 export const GRAPHQL_URL = '/graphql'
-export const LOGIN_URL = 'https://discord.com/api/oauth2/authorize?client_id=762649011404406814&redirect_uri=http%3A%2F%2Ftest.pikodev.me%3A3000%2Fauth%2Fcallback&response_type=code&scope=identify guilds'
-export const INVITE_URL = 'https://discord.com/api/oauth2/authorize?client_id=762649011404406814&scope=bot&response_type=code&permissions=8&redirect_uri=http://test.pikodev.me:3000/invite/callback'
+export const LOGIN_URL = 'https://discord.com/api/oauth2/authorize?client_id=762649011404406814&redirect_uri=http%3A%2F%2F59.19.74.105%3A3000%2Fauth%2Fcallback&response_type=code&scope=identify guilds'
+export const INVITE_URL = 'https://discord.com/api/oauth2/authorize?client_id=762649011404406814&scope=bot&response_type=code&permissions=8&redirect_uri=http://59.19.74.105:3000/invite/callback'
