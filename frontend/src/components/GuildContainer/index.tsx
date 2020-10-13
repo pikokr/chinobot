d2b0a1b88ec224e6db3ec26566b4b0898dd22c05
pikokr <parangee9706@gmail.com>
2020-10-13 18:57:52 +0900

    [Feat] API 오류 발생 케이스 추가

--- frontend/src/components/GuildContainer/index.tsx
+++ frontend/src/components/GuildContainer/index.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Avatar, Card, CardContent, CardHeader, IconButton} from "@material-ui/core";
+import {Avatar, Card, CardContent, CardHeader, IconButton, Typography} from "@material-ui/core";
 import DefaultIcon from '../../assets/img/icon-default.png'
 import {Menu as MenuIcon} from "@material-ui/icons";
 import ServerDrawer from "../../views/Servers/Dashboard/ServerDrawer";
@@ -16,16 +16,42 @@ class GuildContainer extends Component<{
 
         return (
             <Card>
-                <CardHeader title={guild.name} avatar={
-                    <Avatar
-                        src={guild.icon ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}` : DefaultIcon}/>
-                } action={<IconButton onClick={() => this.setState({drawer: true})}>
-                    <MenuIcon/>
-                </IconButton>}/>
-                <ServerDrawer guild={guild} open={this.state.drawer} onClose={() => this.setState({drawer: false})}/>
-                <CardContent>
-                    {this.props.children}
-                </CardContent>
+                {
+                    guild === null ? '로딩중..' : guild === false ? <CardContent>
+                        이 페이지는 다음 이유로 표시되지 않았습니다.
+                        <Typography>
+                            서버가 존재하지 않음
+                        </Typography>
+                        <Typography>
+                            로그인하지 않음
+                        </Typography>
+                        <Typography>
+                            봇이 서버에 없음
+                        </Typography>
+                        <Typography>
+                            유저에게 관리자 권한이 없음
+                        </Typography>
+                        <Typography>
+                            해당 서버를 핸들중인 샤드가 서버에 없음
+                        </Typography>
+                        <Typography>
+                            API 서버에 오류가 발생함
+                        </Typography>
+                    </CardContent> : (
+                        <>
+                            <CardHeader title={guild.name} avatar={
+                                <Avatar
+                                    src={guild.icon ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}` : DefaultIcon}/>
+                            } action={<IconButton onClick={() => this.setState({drawer: true})}>
+                                <MenuIcon/>
+                            </IconButton>}/>
+                            <ServerDrawer guild={guild} open={this.state.drawer} onClose={() => this.setState({drawer: false})}/>
+                            <CardContent>
+                                {this.props.children}
+                            </CardContent>
+                        </>
+                    )
+                }
             </Card>
         );
     }
--- frontend/src/views/Servers/Dashboard/index.tsx
+++ frontend/src/views/Servers/Dashboard/index.tsx
@@ -38,30 +38,7 @@ class Dashboard extends Component<any> {
 
         return (
             <Layout>
-                {
-                    guild === null ? '로딩중..' : guild === false ? <div>
-                        이 페이지는 다음 이유로 표시되지 않았습니다.
-                        <Typography>
-                            서버가 존재하지 않음
-                        </Typography>
-                        <Typography>
-                            로그인하지 않음
-                        </Typography>
-                        <Typography>
-                            봇이 서버에 없음
-                        </Typography>
-                        <Typography>
-                            유저에게 관리자 권한이 없음
-                        </Typography>
-                        <Typography>
-                            해당 서버를 핸들중인 샤드가 서버에 없음
-                        </Typography>
-                    </div> : (
-                        <GuildContainer guild={guild}>
-                            테스트
-                        </GuildContainer>
-                    )
-                }
+                <GuildContainer guild={guild}/>
             </Layout>
         );
     }
