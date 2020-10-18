5b2d7bceee7aea191bb24907dd2a7324adb2c878
pikokr <parangee9706@gmail.com>
2020-10-18 17:37:32 +0900

    [Fix] Lint

--- bot/commands/ music/now.ts
+++ bot/commands/ music/now.ts
@@ -17,24 +17,17 @@ export default class Help extends Command {
 
         if (!player || !player.queue.current) return msg.util!.send(msg.embed().setFooter('').setTitle('재생중인 곡이 없어요!'))
 
-        const embed = msg.embed()
-
         const t = player.queue.current
 
+        const embed = msg.embed()
         embed.setTitle(t.title)
-
         embed.setURL(t.uri)
-
         embed.addField('길이', t.isStream ? '실시간' : formatTime(t.duration), true)
-
         embed.addField('저자', t.author, true)
-
         embed.addField('신청자', (t.requester as User).tag, true)
-
         if (!t.isStream) {
             embed.setDescription('```\n' + `${formatTime(player.position)}/${formatTime(t.duration)}\n` + createBar(t.duration, player.position)[0] + '```')
         }
-
         embed.setImage(t.displayThumbnail('maxresdefault'))
 
         return msg.util!.send(embed)
