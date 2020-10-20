bc1e6127b578270b93ef1189b1e5f6bcc199b8ae
pikokr <parangee9706@gmail.com>
2020-10-20 19:36:23 +0900

    [Fix] Lint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Router from "./router";
 import {CssBaseline} from "@material-ui/core";
-import Layout from "./components/Layout";
 
 const App = () => {
     return (
