972b76c4e94bbb7dcc1ab65909efdb7310963ae7
pikokr <parangee9706@gmail.com>
2020-10-23 20:16:20 +0900

    [Feat] Uptime command

--- bot/commands/general/uptime.ts
+++ bot/commands/general/uptime.ts
@@ -1,5 +1,6 @@
 import {Command} from "discord-akairo";
 import {Message} from "discord.js";
+import moment from 'moment'
 
 export default class Uptime extends Command {
     constructor() {
@@ -10,5 +11,10 @@ export default class Uptime extends Command {
         });
     }
     async exec(msg: Message) {
+        await msg.util!.send(msg.embed().setTitle('치노봇의 업타입!').setDescription([
+            `클라이언트 업타임: ${moment.utc(moment.duration(this.client.uptime).asMilliseconds()).format('HH시간 mm분 ss초')}`,
+            `프로세스 업타임: ${moment.utc(moment.duration(process.uptime() * 1000).asMilliseconds()).format('HH시간 mm분 ss초')}`,
+            `서버 업타임: ${moment.utc(moment.duration(require('os').uptime() * 1000).asMilliseconds()).format('HH시간 mm분 ss초')}`
+        ]))
     }
 }
