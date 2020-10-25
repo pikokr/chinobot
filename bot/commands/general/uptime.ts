e5fa1f50cc99f2e14573d016d1791326f0677423
파랑이 <parangee9706@gmail.com>
2020-10-25 09:50:27 +0900

    Update uptime.ts

--- bot/commands/general/uptime.ts
+++ bot/commands/general/uptime.ts
@@ -11,7 +11,7 @@ export default class Uptime extends Command {
         });
     }
     async exec(msg: Message) {
-        const format = 'HH[시간] mm[분] ss[초]'
+        const format = 'DD[일] HH[시간] mm[분] ss[초]'
         await msg.util!.send(msg.embed().setTitle('치노봇의 업타입!').setDescription([
             `클라이언트 업타임: ${moment.utc(moment.duration(this.client.uptime).asMilliseconds()).format(format)}`,
             `프로세스 업타임: ${moment.utc(moment.duration(process.uptime() * 1000).asMilliseconds()).format(format)}`,
